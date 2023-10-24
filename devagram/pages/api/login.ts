45e18e069fc3c81130fd040c6c35058692235f2b
alfredoepq <alfredoepq@hotmail.com>
2023-10-24 11:44:55 -0300

    Verificando o usuário logado no banco de dados

--- devagram/pages/api/login.ts
+++ devagram/pages/api/login.ts
@@ -1,13 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import conectarMongoDb from '../../models/conectarMongoDb'
+import conectarMongoDb from '../../middlewares/conectarMongoDb'
+import { RespostaPadrao } from "@/types/RespostaPadrao";
+import { UsuarioModel } from "@/models/UsuarioModel";
+import md5 from "md5";
 
-const login = (req: NextApiRequest, res: NextApiResponse) => {
+const login = async (req: NextApiRequest, res: NextApiResponse<RespostaPadrao>) => {
   if (req.method === 'POST') {
     const { login, senha } = req.body;
 
-    if (login === 'admin@123' && senha==='senha@123') {
-      return res.status(200).json({Erro: 'Usuário logado com sucesso'});
+    const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
+    if (usuariosEncontrados && usuariosEncontrados.length > 0) {
+      const usuarioEncontrado = usuariosEncontrados[0];
+
+      res.status(201).json({Mensagem: `O usuário logado é o ${usuarioEncontrado.nome}`})
     }
+
     return res.status(405).json({Erro: 'Senha e login não conferem'});
   }
   return res.status(405).json({Erro: 'Método inválido'});
